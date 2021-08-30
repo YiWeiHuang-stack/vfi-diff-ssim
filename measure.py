38f136ddb0c10f3e3172b13e6397058d05442146
YiWeiHuang-stack <68835291+YiWeiHuang-stack@users.noreply.github.com>
2021-08-30 20:28:01 +0800

    Update measure.py

--- measure.py
+++ measure.py
@@ -1,12 +1,12 @@
 import cv2
 from skimage.measure._structural_similarity import structural_similarity as compare_ssim
 
-# 计算指标
 def calc_index(i0,i1,interp):
     left_diff = 1 - compare_ssim(interp,i0,multichannel=True)
     right_diff = 1 - compare_ssim(interp,i1,multichannel=True)
     return abs(left_diff - right_diff)
 
-im1 = cv2.imread('')
-im2 = cv2.imread('')
-print(calc_index(im1,im2))
+im1 = cv2.imread('origin_frame1.png')
+interp = cv2.imread('interp.png')
+im2 = cv2.imread('origin_frame2.png')
+print(calc_index(im1,im2,interp))
